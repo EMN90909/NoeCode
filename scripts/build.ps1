9a2d1328bf9c18fe862f5e8f11ee06dfcb98d1a8
EMN <nasongoemmanuel8@gmail.com>
2026-09-12 21:59:47 +0300

    Add Noqeri fox to PowerShell build

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,6 +1,21 @@
 $ErrorActionPreference = 'Stop'
+
+$Fox = @'
+        /\       /\
+       /  \_____/  \
+      /   / </> \   \
+     /   /   ^   \   \
+    (    \  ___  /    )
+     \    '.___.'    /
+      '._         _.'
+         '-.___.-'
+           NOQERI
+'@
+Write-Host $Fox
+
 $BuildDir = if ($env:BUILD_DIR) { $env:BUILD_DIR } else { 'build' }
 $BuildType = if ($env:BUILD_TYPE) { $env:BUILD_TYPE } else { 'Release' }
+Write-Host "building Noqeri bootstrap ($BuildType)"
 cmake -S . -B $BuildDir -DCMAKE_BUILD_TYPE=$BuildType
 cmake --build $BuildDir --config $BuildType --parallel
 Write-Host "noqeri bootstrap built in $BuildDir"

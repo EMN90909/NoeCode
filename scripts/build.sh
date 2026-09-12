c433591161f661c06bc552b1615d191d849a70e9
EMN <nasongoemmanuel8@gmail.com>
2026-09-12 21:59:40 +0300

    Add Noqeri fox to Unix build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env sh
 set -eu
+
+cat <<'FOX'
+        /\       /\
+       /  \_____/  \
+      /   / </> \   \
+     /   /   ^   \   \
+    (    \  ___  /    )
+     \    '.___.'    /
+      '._         _.'
+         '-.___.-'
+           NOQERI
+FOX
+
 BUILD_DIR="${BUILD_DIR:-build}"
 BUILD_TYPE="${BUILD_TYPE:-Release}"
+printf 'building Noqeri bootstrap (%s)\n' "$BUILD_TYPE"
 cmake -S . -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
 cmake --build "$BUILD_DIR" --config "$BUILD_TYPE" --parallel
 printf 'noqeri bootstrap built in %s\n' "$BUILD_DIR"
